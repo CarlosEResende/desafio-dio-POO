44c0276d1316b823d68db766e04e18a1291d90d2
Carlos Eduardo <carlozresende@gmail.com>
2024-07-22 22:03:59 -0300

    Metodo Conteudo Abstract

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo {
+
+    private int CargaHoraria;
+
+    public Curso() {
+
+    }
+
+    @Override
+    public double calcularXp() {
+
+        return XP_PADRAO * CargaHoraria;
+    }
+
+    public int getCargaHoraria() {
+        return CargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        CargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Conteudo [titulo=" + getTitulo() + ", descricao=" + getDescricao() + "carga horaria=" + CargaHoraria
+                + "]";
+
+    }
+
+}
